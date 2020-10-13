a3ec45087087ee8f0abe7059ea263b4b1feb35e1
naimtahouri <naimtahouri@gmail.com>
2020-10-13 14:50:26 +0200

    signin component and signup component validé + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: SigninComponent,
+	},
+	{
+		path: 'signup',
+		component: SignupComponent,
+		pathMatch: 'prefix',
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
